a37099aeef078da0015a33e90866f11d2fc24bb0
ElCurdente <jules.mesnil@gmail.com>
2022-02-16 17:01:21 +0100

    lets gooo

--- App.js
+++ App.js
@@ -1,22 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import benef from './img/logo_benef.svg';
+import tw from 'tailwind-react-native-classnames';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>BENEF</Text>
-      <img src={benef}/>
+    <View style={tw`flex-1 justify-center items-center`}>
+      <Text>Château de Champs-Sur-Marne</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ff5640',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
