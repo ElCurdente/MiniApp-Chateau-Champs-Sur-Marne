db28a774733d65982e243acee3c9632b1663a62c
ElCurdente <jules.mesnil@gmail.com>
2022-02-16 16:12:45 +0100

    lets go

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import benef from './img/logo_benef.svg';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>BENEF</Text>
+      <img src={benef}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ff5640',
     alignItems: 'center',
     justifyContent: 'center',
   },
