108f4d2bc7febe81a47b0ca0a19690bd79bc7c57
ElCurdente <jules.mesnil@gmail.com>
2022-02-16 17:49:05 +0100

    test img

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import tw from 'tailwind-react-native-classnames';
+import chateau from './img/chateau.jpg';
 
 export default function App() {
   return (
     <View style={tw`flex-1 justify-center items-center`}>
       <Text>Château de Champs-Sur-Marne</Text>
+      <img src={chateau} style={tw`rounded-xl`} />
       <StatusBar style="auto" />
     </View>
   );
