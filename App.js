ac3da48d3a47d61fb0868b94df3f93d86f9524f9
ElCurdente <jules.mesnil@gmail.com>
2022-03-17 13:57:09 +0100

    react

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import {Text, View} from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import tw from 'tailwind-react-native-classnames';
