426089d5c7543cf5930763645333028c04cfd5df
Bigou <antoine.droyer@gmail.com>
2022-03-22 14:31:38 +0100

    quizz

--- Quizz.js
+++ Quizz.js
@@ -12,6 +12,9 @@ export default function Quizz() {
   const [isLoaded, setIsLoaded] = useState(false);
   const [questions, setQuestions] = useState([]);
   const [answers, setAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [numberAnswer, setNumberAnswer] = useState(0)
+
 
   useEffect(() => {
     fetch("https://api-miniapp.antoine-droyer.fr/api-questions.php")
@@ -43,27 +46,56 @@ export default function Quizz() {
       )
   },[])
 
+  function onPressAnswer() {
+    if(this.type == 1) {
+      setScore(score + 1);
+      console.log(this)
+    }
+    setAnswers(prevState => [...prevState, this.ext_id_question]);
+    setNumberAnswer(numberAnswer + 1);
+  }
+
+
   let answersTemp
 
   return (
     <View style={tw`flex-1 justify-center items-center`}>
+      <Text>Quizz</Text>
       {questions.map(q => (
         answersTemp = answers.filter(x => x.ext_id_question == q.id_question),
         <View>
       <Text>{q.name}</Text>
       {answersTemp.map(a => (
         <View>
+          {answers.find(x => x == q.id_question) ?
           <Button
-            
-            title={a.contenu}
-            color="#841584"
-            accessibilityLabel="Learn more about this purple button"
-          />
+          onPress={onPressAnswer.bind(a)}
+          title={a.contenu}
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+          disabled="true"
+        />
+        :
+        <Button
+        onPress={onPressAnswer.bind(a)}
+        title={a.contenu}
+        color="#f1f1f1"
+        accessibilityLabel="Learn more about this purple button"
+        />
+        }
         </View>  
         ))}
         </View>
     ))}
-
+    <Text>Score : {score}</Text>
+    {numberAnswer == 7 && 
+    // <Text>Quizz terminé !</Text>
+    <Button
+    title="Partager"
+    color="#841584"
+    accessibilityLabel="Learn more about this purple button"
+  />
+    }
       <StatusBar style="auto" />
     </View>
   );
